a9eedec49aacf60d399c74e22e513ccb07924e67
Gustavo Pereira <gustavro33@gmail.com>
2024-09-10 20:03:12 -0300

    terceiro

--- src/main/java/com/example/crud_funcionarios/model/Funcionario.java
+++ src/main/java/com/example/crud_funcionarios/model/Funcionario.java
@@ -2,6 +2,7 @@ package com.example.crud_funcionarios.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.NotFound;
 
 import java.util.Date;
 
@@ -14,21 +15,27 @@ public class Funcionario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotFound
     @Column(name = "nome", length = 100)
     private String nome;
 
+    @NotFound
     @Column(name = "cargo", length = 50)
     private String cargo;
 
+    @NotFound
     @Column(name = "departamento", length = 50)
     private String departamento;
 
+    @NotFound
     @Column(name = "data_admissao")
     private Date dataAdmissao;
 
+    @NotFound
     @Column(name = "email", length = 100)
     private String email;
 
+    @NotFound
     @Column(name = "telefone", length = 20)
     private String telefone;
 }
